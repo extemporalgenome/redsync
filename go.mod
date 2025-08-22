da58a83723d929bb05090479a79cfd4e69cea742
Kevin Gillette <kgillette@twilio.com>
2025-08-22 15:17:51 -0600

    code simplifications part 1

--- go.mod
+++ go.mod
@@ -1,6 +1,6 @@
 module github.com/go-redsync/redsync/v4
 
-go 1.23.0
+go 1.24
 
 require (
 	github.com/go-redis/redis v6.15.9+incompatible
--- mutex.go
+++ mutex.go
@@ -102,9 +102,10 @@ func (m *Mutex) lockContext(ctx context.Context, tries int) error {
 		}
 
 		start := time.Now()
+		adjustedTTL := time.Duration(float64(m.expiry) * m.timeoutFactor)
 
 		n, err := func() (int, error) {
-			ctx, cancel := context.WithTimeout(ctx, time.Duration(int64(float64(m.expiry)*m.timeoutFactor)))
+			ctx, cancel := context.WithTimeout(ctx, adjustedTTL)
 			defer cancel()
 			return m.actOnPoolsAsync(func(pool redis.Pool) (bool, error) {
 				return m.acquire(ctx, pool, value)
@@ -112,16 +113,17 @@ func (m *Mutex) lockContext(ctx context.Context, tries int) error {
 		}()
 
 		now := time.Now()
-		until := now.Add(m.expiry - now.Sub(start) - time.Duration(int64(float64(m.expiry)*m.driftFactor)))
+		until := now.Add(m.expiry - now.Sub(start) - adjustedTTL)
 		if n >= m.quorum && now.Before(until) {
 			m.value = value
 			m.until = until
 			return nil
 		}
-		_, _ = func() (int, error) {
-			ctx, cancel := context.WithTimeout(ctx, time.Duration(int64(float64(m.expiry)*m.timeoutFactor)))
+
+		func() {
+			ctx, cancel := context.WithTimeout(ctx, adjustedTTL)
 			defer cancel()
-			return m.actOnPoolsAsync(func(pool redis.Pool) (bool, error) {
+			m.actOnPoolsAsync(func(pool redis.Pool) (bool, error) {
 				return m.release(ctx, pool, value)
 			})
 		}()
